aee24a34e0b2d7874262fbd11f7eb61a469fd8a9
Ryosuke Shiroshita <admin@qurore.com>
2025-02-11 19:49:06 -0800

    modified build gradle compile SDK version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.github.qurore.fittrack"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.github.qurore.fittrack"
